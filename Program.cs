f5acf5207983232a6bef9de6851565917aadb144
Vitor <vitormateushd123@gmail.com>
2025-09-24 16:52:21 -0300

    Update Program.cs
    swagger normal de volta

--- Program.cs
+++ Program.cs
@@ -16,10 +16,7 @@ var app = builder.Build();
 //app.Urls.Add($"http//*:{port}");
 
 app.UseSwagger();
-app.UseSwaggerUI(c => {
-  c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API v1");
-  c.RoutePrefix = string.Empty;
-});
+app.UseSwaggerUI();
 
 app.PessoaRoutes();
 
